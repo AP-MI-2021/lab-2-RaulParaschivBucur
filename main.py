a247e30295006e81138db568fef6dc880ce909b6
RaulParaschivBucur <91596727+RaulParaschivBucur@users.noreply.github.com>
2021-10-06 12:16:54 +0300

    Ch 5

--- main.py
+++ main.py
@@ -31,6 +31,37 @@ def get_perfect_squares(start: int, end: int) -> list[int]:
     return lst
 
 
+def get_temp(temp: float, From: str, to: str) -> float:
+    """
+    Transforma o temperatura data intr-o scara data
+    Input: Temperatura, scara in care este, scara in care vrem sa o transformam --> temp, From, to
+    Output: Valoarea temperaturii dupa transformarea in scara aleasa in variabila ''to''
+    """
+    if (From == 'C') and (to == 'F'):
+        return temp * (9 / 5) + 32
+    if (From == 'F') and (to == 'C'):
+        return (temp - 32) * (5 / 9)
+
+    if (From == 'C') and (to == 'K'):
+        return temp + 273.15
+    if (From == 'K') and (to == 'C'):
+        return temp - 273.15
+
+    if (From == 'F') and (to == 'K'):
+        return (temp - 32) * (5 / 9) + 273.15
+    if (From == 'K') and (to == 'F'):
+        return (temp - 273.15) * (9 / 5) + 32
+
+
+def test_get_temp():
+    assert "{:.2f}".format(get_temp(300, 'K', 'C')) == '26.85'
+    assert "{:.2f}".format(get_temp(0, 'C', 'K')) == '273.15'
+    assert "{:.2f}".format(get_temp(100, 'F', 'C')) == '37.78'
+    assert "{:.2f}".format(get_temp(100, 'C', 'F')) == '212.00'
+    assert "{:.2f}".format(get_temp(120, 'F', 'K')) == '322.04'
+    assert "{:.2f}".format(get_temp(120, 'K', 'F')) == '-243.67'
+
+
 def test_get_leap_years():
     assert get_leap_years(2000, 2021) == [2000, 2004, 2008, 2012, 2016, 2020]
     assert get_leap_years(0, 0) == []
@@ -49,7 +80,8 @@ def main():
         print(' ')
         print('1. Afiseaza toti anii bisecti intre 2 ani dati,inclusiv anii dati')
         print('2. Afiseaza toate patratele perfecte dintr-un interval inchis dat')
-        print('3. Iesi')
+        print('3. Transforma o temperatura intr-o scara aleasa')
+        print('4. Iesi')
         optiune = input('Alege o optiune: ')
 
         if optiune == '1':
@@ -73,9 +105,15 @@ def main():
                 print(result)
 
         elif optiune == '3':
+            temp = float(input('Alege temperatura:'))
+            From = str(input('Alege scara initiala:'))
+            to = str(input('Alege scara in care vrei sa transformi:'))
+            print('Valoarea dupa transformare este:', "{:.2f}".format(get_temp(temp, From, to)))
+        elif optiune == '4':
             break
 
 
 test_get_leap_years()
 test_get_perfect_squares()
+test_get_temp()
 main()
